b71e71f1f99168fd77a2bd0ec0c63fe882300f6e
Rafay <abdulrafay6456@gmail.com>
2024-07-27 15:14:49 +0500

    included the depedencies and permission

--- Api_to_recyclerview/app/src/main/java/com/example/destination/ui/main/MainActivity.kt
+++ Api_to_recyclerview/app/src/main/java/com/example/destination/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.destination
+package com.example.destination.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.destination.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
