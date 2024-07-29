3838a5a572f962f0bb4f35e3f404c60ea507e0a1
Rafay <abdulrafay6456@gmail.com>
2024-07-29 13:52:38 +0500

    three compoents (image name and description)

--- Api_to_recyclerview/app/build.gradle.kts
+++ Api_to_recyclerview/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.retrofit)
     implementation (libs.converter.gson)
+    implementation (libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Api_to_recyclerview/app/src/main/java/com/example/destination/data/adapter/RecyclerViewAdapter.kt
+++ Api_to_recyclerview/app/src/main/java/com/example/destination/data/adapter/RecyclerViewAdapter.kt
@@ -1,10 +1,16 @@
 package com.example.destination.data.adapter
 
+import android.graphics.Typeface
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.destination.R
 import com.example.destination.data.model.Destination
 
@@ -13,6 +19,8 @@ class RecyclerViewAdapter(private val mList: List<Destination>) :
 
     class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         val name: TextView = this.itemView.findViewById(R.id.name)
+        val description: TextView = this.itemView.findViewById(R.id.description)
+        val image: ImageView = this.itemView.findViewById(R.id.image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,15 +30,20 @@ class RecyclerViewAdapter(private val mList: List<Destination>) :
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val destination = mList[position]
-
-        holder.name.text = destination.name
-
-    }
-
     override fun getItemCount(): Int {
         return mList.size
     }
 
-}
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val destination = mList[position]
+
+        holder.name.text = destination.name
+        val descriptionText = SpannableStringBuilder().apply {
+            append("Description:\n", StyleSpan(Typeface.BOLD), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            append(destination.description)
+        }
+        holder.description.text = descriptionText
+        Glide.with(holder.image.context).load(destination.image).into(holder.image)
+    }
+
+    }
